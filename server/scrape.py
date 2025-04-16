d9ff6bf59bb0a531d434ced36fd1a55f77e01443
georgejmx <georgejm2358@protonmail.com>
2025-04-16 20:15:41 +0200

    feat: non-naive html parsing

--- server/scrape.py
+++ server/scrape.py
@@ -2,6 +2,36 @@ from httpx import AsyncClient
 from bs4 import BeautifulSoup
 
 
+RELEVANT_TAGS = [
+    "header",
+    "footer",
+    "main",
+    "nav",
+    "section",
+    "article",
+    "aside",
+    "div",
+    "h1",
+    "h2",
+    "h3",
+    "p",
+    "ul",
+    "ol",
+    "li",
+    "blockquote",
+    "a",
+    "img",
+    "video",
+    "audio",
+    "iframe",
+    "form",
+    "input",
+    "button",
+    "select",
+    "textarea",
+]
+
+
 async def fetch_site(url: str, timestamp: int) -> str:
     """Call the Wayback Machine for raw html at the given timestamp"""
     async with AsyncClient() as client:
@@ -9,10 +39,19 @@ async def fetch_site(url: str, timestamp: int) -> str:
         response = await client.get(wayback_url)
         return response.text
 
+
 async def scrape_site(url: str, timestamp: int) -> str:
     """Scrapes a single site given a timestamp and its raw html"""
     html = await fetch_site(url, timestamp)
     soup = BeautifulSoup(html, "html.parser")
+    body = soup.find("body")
 
-    text_tags = [tag.name for tag in soup.find_all()]
+    if not body:
+        raise Exception(
+            f"Site with url {url} cannot be parsed or has severely malformed HTML"
+        )
+
+    text_tags = [
+        tag.name for tag in body.find_all(lambda tag: tag.name in RELEVANT_TAGS)
+    ]
     return " ".join(text_tags)
